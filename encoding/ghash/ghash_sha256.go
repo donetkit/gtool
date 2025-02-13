ab06752683f9bd90cc5a33ef18870168f96f029e
DoNetKit <donetkit@gmail.com>
2025-02-13 10:20:35 +0800

    sha256

---
+++ encoding/ghash/ghash_sha256.go
@@ -0,0 +1,20 @@
+package ghash
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+func Sha256(input string) string {
+	// 创建一个新的 SHA-256 哈希接口
+	data := sha256.New()
+
+	// 将输入字符串写入哈希接口中
+	data.Write([]byte(input))
+
+	// 计算哈希值，并返回其字节切片表示形式
+	hashBytes := data.Sum(nil)
+
+	// 将字节切片转换为十六进制字符串表示形式
+	return hex.EncodeToString(hashBytes)
+}
