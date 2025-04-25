3093cccc3a7bd572cc23381b0a3cee734345a722
DoNetKit <donetkit@gmail.com>
2025-04-25 10:37:17 +0800

    chan

--- container/gchan/gchan_test.go
+++ container/gchan/gchan_test.go
@@ -1,6 +1,7 @@
 package gchan
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"testing"
@@ -51,4 +52,29 @@ func TestBatchProcessorWithTimeout(t *testing.T) {
 	for batch := range BatchProcessorWithTimeout(input, 3, time.Second*3) {
 		log.Println("BatchProcessorWithTimeout:", batch)
 	}
+}
+
+func TestUnboundedChan(t *testing.T) {
+	ctx := context.Background()
+	ch := NewUnboundedChan[int](ctx, 1000)
+	// or ch := NewUnboundedChanSize(10,200,1000)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			ch.In <- i
+			if i == 50 {
+				break
+			}
+		}
+		fmt.Println("Out 0 close(ch.In)")
+		close(ch.In) // close In channel
+	}()
+	fmt.Println("Out 1")
+	for v := range ch.Out { // read values
+		fmt.Println(v)
+	}
+	fmt.Println("Out 2")
+
+	time.Sleep(time.Second * 1115)
+
 }
